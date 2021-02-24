b26faafb5122ac9c59e5f5bd3cc8a3fef5a51582
e-amce <adam.crow@gft.com>
2021-02-24 14:52:48 +0000

    - Update

--- examples/simple/networking.tf
+++ examples/simple/networking.tf
@@ -17,6 +17,7 @@ resource "google_compute_firewall" "allow_iap_ssh" {
   network     = google_compute_network.network.self_link
   source_ranges = ["35.235.240.0/20"]
   target_tags = ["allow-iap-ssh"]
+  project     = var.project_id
   allow {
     protocol = "tcp"
     ports    = ["22"]
