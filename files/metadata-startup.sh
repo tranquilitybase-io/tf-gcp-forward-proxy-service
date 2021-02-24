2addc94b882519f1306c8459fbe4c7f48e828aef
nhsy <narish.samplay@gft.com>
2021-02-24 15:33:54 +0000

    updated metadata-startup.sh

--- files/metadata-startup.sh
+++ files/metadata-startup.sh
@@ -7,8 +7,6 @@ echo "metadata_start"
 
 apt install -y squid
 
-#Add IAP source CIDR
-echo "acl localnet src 35.235.240.0/20" | tee -a /etc/squid/squid.conf
 systemctl enable squid
 systemctl start squid
 
