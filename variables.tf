5d8ad49842e95595992320b0700788213b25c0f2
nhsy <narish.samplay@gft.com>
2021-02-25 09:58:21 +0000

    Added healthcheck fw rules

--- variables.tf
+++ variables.tf
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "target_tags" {
-  description = "Target tags"
+variable "tags" {
+  description = "Firewall tags"
   type        = list(any)
   default     = []
 }
