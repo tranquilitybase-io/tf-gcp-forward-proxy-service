9dc011d215d12c2b6d5e022d72f246720197b2eb
amce <amce@gftdevgcp.com>
2021-03-29 17:31:37 +0100

    Add forward_proxy_name

--- datasources.tf
+++ datasources.tf
@@ -4,10 +4,16 @@ data "google_compute_zones" "available" {
   status  = "UP"
 }
 
+resource "time_sleep" "wait_120_seconds" {
+  depends_on = [module.mig]
+
+  create_duration = "120s"
+}
+
 data "google_compute_instance_group" "mig_instances" {
   name    = format("%s-%s", var.mig_hostname, "mig")
   project = var.project_id
   zone    = data.google_compute_zones.available.names[0]
 
-  depends_on = [module.mig]
+  depends_on = [time_sleep.wait_120_seconds]
 }
