582a46f552eb7625b1f1e49d0f3629279fb5bb3e
amce <amce@gftdevgcp.com>
2021-03-31 15:07:57 +0100

    test

--- mig.tf
+++ mig.tf
@@ -57,12 +57,12 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 
   network_interface {
-    network = var.network_name
+    network = "tb-mgmt-network"
   }
 
   metadata_startup_script = file("${path.module}/files/metadata-startup.sh")
