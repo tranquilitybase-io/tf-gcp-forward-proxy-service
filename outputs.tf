a1eb6c12b15a96e1ef679d1880691c606e6b0f16
amce <amce@gftdevgcp.com>
2021-03-31 12:25:11 +0100

    edit to output foward proxy name

--- outputs.tf
+++ outputs.tf
@@ -41,5 +41,5 @@ output "mig_name" {
 }
 
 output "forward_proxy_name" {
-  value = data.google_compute_instance_group.mig_instances.instances[0]
+  value = tolist(data.google_compute_instance_group.mig_instances.instances)
 }
