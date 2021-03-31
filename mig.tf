216c37c8af72f2d80de0834963b47da2d2ea48a7
amce <amce@gftdevgcp.com>
2021-03-31 14:50:18 +0100

    edit to output foward proxy name

--- mig.tf
+++ mig.tf
@@ -12,39 +12,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-module "instance-template" {
-  source  = "terraform-google-modules/vm/google//modules/instance_template"
-  version = "6.0.0"
+//module "instance-template" {
+//  source  = "terraform-google-modules/vm/google//modules/instance_template"
+//  version = "6.0.0"
+//
+//  disk_size_gb         = var.disk_size_gb
+//  machine_type         = var.machine_type
+//  name_prefix          = var.template_name
+//  network              = var.network_name
+//  preemptible          = var.preemptible
+//  project_id           = var.project_id
+//  region               = var.region
+//  service_account      = local.service_account_object
+//  source_image_family  = var.source_image_family
+//  source_image_project = var.source_image_project
+//  startup_script       = file("${path.module}/files/metadata-startup.sh")
+//  subnetwork           = var.subnet_name
+//  subnetwork_project   = var.project_id
+//  tags                 = var.tags
+//
+//  depends_on = [module.service-account]
+//}
+//
+//module "mig" {
+//  source  = "terraform-google-modules/vm/google//modules/mig"
+//  version = "6.0.0"
+//
+//  distribution_policy_zones = [data.google_compute_zones.available.names[0]]
+//  health_check              = var.health_check
+//  hostname                  = var.mig_hostname
+//  instance_template         = module.instance-template.self_link
+//  network                   = var.network_name
+//  project_id                = var.project_id
+//  region                    = var.region
+//  subnetwork                = var.subnet_name
+//  target_size               = var.node_count
+//}
 
-  disk_size_gb         = var.disk_size_gb
-  machine_type         = var.machine_type
-  name_prefix          = var.template_name
-  network              = var.network_name
-  preemptible          = var.preemptible
-  project_id           = var.project_id
-  region               = var.region
-  service_account      = local.service_account_object
-  source_image_family  = var.source_image_family
-  source_image_project = var.source_image_project
-  startup_script       = file("${path.module}/files/metadata-startup.sh")
-  subnetwork           = var.subnet_name
-  subnetwork_project   = var.project_id
-  tags                 = var.tags
+resource "google_compute_instance" "default" {
+  name         = "forward-proxy-instance"
+  machine_type = "e2-medium"
+  zone         = "europe-west1"
 
-  depends_on = [module.service-account]
-}
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 
-module "mig" {
-  source  = "terraform-google-modules/vm/google//modules/mig"
-  version = "6.0.0"
+  network_interface {
+    network = var.network_name
+  }
 
-  distribution_policy_zones = [data.google_compute_zones.available.names[0]]
-  health_check              = var.health_check
-  hostname                  = var.mig_hostname
-  instance_template         = module.instance-template.self_link
-  network                   = var.network_name
-  project_id                = var.project_id
-  region                    = var.region
-  subnetwork                = var.subnet_name
-  target_size               = var.node_count
+  metadata_startup_script = file("${path.module}/files/metadata-startup.sh")
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = local.sa_name
+    scopes = ["cloud-platform"]
+  }
 }
