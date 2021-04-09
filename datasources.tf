e4447c8b705fcb30c4a7d422ce5a8b5c4cc83ac2
amce <amce@gftdevgcp.com>
2021-04-09 14:48:22 +0100

    remove unneeded code

--- datasources.tf
+++ datasources.tf
@@ -18,16 +18,16 @@ data "google_compute_zones" "available" {
   status  = "UP"
 }
 
-resource "time_sleep" "wait_120_seconds" {
-  depends_on = [module.mig]
-
-  create_duration = "1s"
-}
-
-data "google_compute_instance_group" "mig_instances" {
-  name    = format("%s-%s", var.mig_hostname, "mig")
-  project = var.project_id
-  zone    = data.google_compute_zones.available.names[0]
-
-  depends_on = [time_sleep.wait_120_seconds]
-}
+//resource "time_sleep" "wait_120_seconds" {
+//  depends_on = [module.mig]
+//
+//  create_duration = "1s"
+//}
+//
+//data "google_compute_instance_group" "mig_instances" {
+//  name    = format("%s-%s", var.mig_hostname, "mig")
+//  project = var.project_id
+//  zone    = data.google_compute_zones.available.names[0]
+//
+//  depends_on = [time_sleep.wait_120_seconds]
+//}
