539db51cb18a3b95e451451136c30979ffe8d09d
e-amce <adam.crow@gft.com>
2021-06-09 15:04:57 +0100

    test

--- variables.tf
+++ variables.tf
@@ -98,7 +98,7 @@ variable "service_account_name" {
 variable "source_image_family" {
   description = "Source image family for template"
   type        = string
-  default     = "debian-9"
+  default     = "debian-10"
 }
 
 variable "source_image_project" {
